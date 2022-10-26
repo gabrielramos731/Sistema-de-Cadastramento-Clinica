afc22eb0eb3f5da029e2868cd14583f82d373429
Gabriel Ramos <ramos.gabrielsilva23@gmail.com>
2022-10-26 15:33:01 -0300

    struct e variaveis

--- main.c
+++ main.c
@@ -6,13 +6,24 @@ typedef struct{
 	char senha[10];
 } login;
 
+typedef struct{
+  char crm[6];
+  char nome[50];
+  char especialidade[20];
+  char dataDeNascimento[10];
+  char telefone[11];
+  float valorHoraTrabalho;
+} medico;
+
 void verificarLogin(FILE *ponteiroLogin, int *contadorTentativas,  int *acesso);
 void cadastrarNovoUsuario(FILE *ponteiroLogin);
 
 int main(){
 	FILE *ponteiroLogin;
+	FILE *ponteiroMedicos;
 	int opcao, contadorTentativas = 0, acesso = 0;
 
+  //login e cadastro
 	do{
 		printf("\n***** MENU *****\n");
 		printf("(1) Efetuar login\n(2) Cadastrar usuario\n(0) Encerrar programa\n");
@@ -44,7 +55,33 @@ int main(){
 		}
 	} while (acesso != 1);
 
-	printf("login aceito");  //as outras opções vem agora
+  //medico, usuario e paciente
+	do{
+
+    printf("Insira a opcao desejada: ");
+    printf("\n(1) Medico");
+    printf("\n(2) Paciente");
+    printf("\n(3) Consulta");
+    printf("\n(0) Encerra programa\n");
+
+    scanf("%d", &opcao);
+    switch(opcao){
+	  	case 1:
+		  	printf("Entrando na seccao medicos...");
+		  	break;
+			case 2:
+		    printf("Entrando na seccao paciente...");
+				break;
+			case 3:
+				printf("Entrando na seccao consultas...");
+   	 		break;
+    	case 0:
+      	printf("\nFechando programa...\n");
+      	break;
+	  	default:
+	  		printf("\nOpcao invalida\n\n");
+		}
+	}while (opcao != 0);
 }
 
 void verificarLogin(FILE *ponteiroLogin, int *contadorTentativas,  int *acesso){
@@ -83,4 +120,3 @@ void cadastrarNovoUsuario(FILE *ponteiroLogin){
 	scanf("%[^\n]s", individuo.senha);
 	fwrite(&individuo, sizeof(login), 1, ponteiroLogin);  //cadastra sempre no final
 }
- 
